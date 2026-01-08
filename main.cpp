f122d8e59550092ef8fa7a6b32e6a14798ff1b77
Rithwik Murthy <86161292+rmwicks12@users.noreply.github.com>
2026-01-09 01:22:17 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#define trigPin 2
+#define echoPin 3
+#define motorPin 4  // Transistor control pin
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(motorPin, OUTPUT);
+}
+
+void loop() {
+  long duration;
+  int distance;
+
+  // Trigger the sensor
+  digitalWrite(trigPin, LOW); delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH); delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  // Read echo
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+
+  // Show distance
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  // Activate motor if object is close
+  if (distance > 0 && distance <= 30) {
+    digitalWrite(motorPin, HIGH); // Turn on vibration
+  } else {
+    digitalWrite(motorPin, LOW);  // Turn off
+  }
+
+  delay(200); // Small delay for stability
+}
